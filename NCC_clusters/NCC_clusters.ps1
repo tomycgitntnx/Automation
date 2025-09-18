75a88b91642460aefc400580617409363054f6a8
tomycgitntnx <alvaro.carrasc@nutanix.com>
2025-09-18 14:14:26 -0400

    Update NCC_clusters.ps1

--- NCC_clusters/NCC_clusters.ps1
+++ NCC_clusters/NCC_clusters.ps1
@@ -1,11 +1,11 @@
-﻿# NCC_clusters.ps1 v1.0  August 2025
+# NCC_clusters.ps1 v1.0  Sept 18, 2025
 # This script runs full NCC checks on all clusters in clusters.txt 
 # This is not a Nutanix Supported script. Do not use to run any config change or disruptive commnads.
 # Usage: .\NCC_clusters.ps1 
 
 
 # Define paths and variables 
-$clusterFile = "C:\Users\xxacarrasco\Documents\Scripts\clusters.txt" 
+$clusterFile = ".\clusters.txt" 
 $plinkPath = "plink.exe"  # Or the full path: "C:\Program Files\PuTTY\plink.exe" 
 $sshUser = "nutanix"       # Change as needed 
 
@@ -17,13 +17,13 @@ $remoteCommand =  $env + ";" + $AOS_command
 
 # Generate a timestamped output file 
 $dateStamp = Get-Date -Format "yyyy_MM_dd__HH_mm_ss" 
-$outputFile = "C:\Users\xxacarrasco\Documents\Scripts\Nutanix_NCC_Clusters_$dateStamp.txt"
+$outputFile = ".\Nutanix_NCC_Clusters_$dateStamp.txt"
 # Setup output file 
 Set-Content -Path $outputFile -Value "________________________ Date: $datestamp ___________________________ `n"
 
 # Check if clusters file exists 
 if (-not (Test-Path $clusterFile)) {
-    Write-Host "File '$clusterFile' not found!" 
+    Write-Host "File '$clusterFile' not found!" -ForegroundColor Red
     exit 1 
 } 
 
@@ -32,7 +32,7 @@ $clusters = Get-Content $clusterFile | Where-Object { $_.Trim() -ne "" }
  
 # Loop through clusters and execute plink 
 foreach ($namecluster in $clusters) { 
-    Write-Host "Connecting to $namecluster..." 
+    Write-Host "Connecting to $namecluster..." -ForegroundColor Yellow
 
     $plinkArgs = @( 
         "-batch"
@@ -45,7 +45,8 @@ foreach ($namecluster in $clusters) {
 
     $result = & $plinkPath @plinkArgs 2>&1 
 
-    # create a separate output file for each cluster !!!!!
+    # Display outputs on screen:
+    $result -split "`n" | ForEach-Object {Write-Host $_ -ForegroundColor Cyan}
 
     # Save output with host header
     Add-Content -Path $outputFile -Value "===== $namecluster =====" 
@@ -53,4 +54,4 @@ foreach ($namecluster in $clusters) {
     Add-Content -Path $outputFile -Value "`n" 
 } 
 
-Write-Host "All done. Output saved to $outputFile" 
+Write-Host "All done. Output saved to $outputFile" -ForegroundColor Green
