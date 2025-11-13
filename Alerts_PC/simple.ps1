b72e9bdcdddce8b64ac352dc0a3cc4ddd43b8125
tomycgitntnx <alvaro.carrasc@nutanix.com>
2025-11-13 16:57:39 -0500

    Create simple.ps1

---
+++ Alerts_PC/simple.ps1
@@ -0,0 +1,23 @@
+# Define your username and password
+$username = "ntx_collector_svc@vrs.state.va.us"
+$password = "M@ryH@d@Littl3L@mb2025!!"
+
+# Base64 encode the credentials
+$base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0}:{1}" -f $username,$password)))
+
+# Construct the Authorization header
+$headers = @{
+    Authorization = "Basic $base64AuthInfo"
+}
+
+# Define the URI of the API endpoint
+$uri = "https://s1-nx-prism:9440/api/monitoring/v4.0.b1/serviceability/alerts"
+
+# Invoke the REST method with the headers
+try {
+    $response = Invoke-RestMethod -Uri $uri -Method Get -Headers $headers
+    $response | ConvertTo-Json -Depth 5 # Display the response (adjust depth as needed)
+}
+catch {
+    Write-Error "Error invoking REST method: $($_.Exception.Message)"
+}
