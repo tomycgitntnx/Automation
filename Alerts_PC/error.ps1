9a629f1e959d2b4549384f3d6976de6586a004f2
tomycgitntnx <alvaro.carrasc@nutanix.com>
2025-11-18 16:11:11 -0500

    Update error.ps1

--- Alerts_PC/error.ps1
+++ Alerts_PC/error.ps1
@@ -1,5 +1,5 @@
 Querying API endpoint: https://s1-nx-prism:9440/api/monitoring/v4.0.b1/alerts?$filter=status ne 'RESOLVED'
-C:\Users\xxacarrasco\Scripting\PS_scripts\Unresolved_alerts\test_invoke_api.ps1 : Error invoking REST method: A parameter cannot be found that matches parameter name 'SkipCertificateCheck'.
+C:\Users\xxacarrasco\Scripting\PS_scripts\Unresolved_alerts\test_invoke_api.ps1 : Error invoking REST method: The underlying connection was closed: An unexpected error occurred on a send.
 At line:1 char:1
 + . 'C:\Users\xxacarrasco\Scripting\PS_scripts\Unresolved_alerts\test_i ...
 + ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
