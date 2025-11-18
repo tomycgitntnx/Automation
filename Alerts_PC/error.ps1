9d09429a11f22d20504ccd840a90ef4567dca872
tomycgitntnx <alvaro.carrasc@nutanix.com>
2025-11-18 16:06:52 -0500

    Update error.ps1

--- Alerts_PC/error.ps1
+++ Alerts_PC/error.ps1
@@ -1,9 +1,7 @@
+Querying API endpoint: https://s1-nx-prism:9440/api/monitoring/v4.0.b1/alerts?$filter=status ne 'RESOLVED'
 C:\Users\xxacarrasco\Scripting\PS_scripts\Unresolved_alerts\test_invoke_api.ps1 : Error invoking REST method: A parameter cannot be found that matches parameter name 'SkipCertificateCheck'.
 At line:1 char:1
 + . 'C:\Users\xxacarrasco\Scripting\PS_scripts\Unresolved_alerts\test_i ...
 + ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     + CategoryInfo          : NotSpecified: (:) [Write-Error], WriteErrorException
     + FullyQualifiedErrorId : Microsoft.PowerShell.Commands.WriteErrorException,test_invoke_api.ps1
-
-
-[System.Net.ServicePointManager]::ServerCertificateValidationCallback = { $true }
